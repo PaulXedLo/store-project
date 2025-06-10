7364beabd55f36bad7ac933d884f565ee6733986
PaulXedLo <lastxedlo@gmail.com>
2025-06-11 02:59:54 +0300

    added home sidebar button

--- src/app/components/sidebar/Menu.tsx
+++ src/app/components/sidebar/Menu.tsx
@@ -1,5 +1,5 @@
 import { Archive as AllItemsIcon } from "lucide-react";
-import { Plus as ListItemIcon } from "lucide-react";
+import { Plus as ListItemIcon, Home } from "lucide-react";
 import Link from "next/link";
 import { NotebookTabs as CurrentlyListedIcon } from "lucide-react";
 import MenuItem from "./MenuItem";
@@ -12,6 +12,9 @@ export default function Menu({
     // Menu component that renders a list of menu items
     // Each menu item is a list item with an icon and label
     <ul className=" mt-7 flex flex-col text-white dark:text-gray-500 ">
+      <Link href="/" onClick={() => setSidebarOpen(false)}>
+        <MenuItem icon={Home} label="Home" />
+      </Link>
       <Link href="/allproducts" onClick={() => setSidebarOpen(false)}>
         <MenuItem icon={AllItemsIcon} label="All items" />
       </Link>
